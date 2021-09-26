e3db7a6c9a4e78fbe7db31b8e71e77fb94c15b56
Bryan <bryan@avalonstar.com>
2021-09-26 12:18:07 -0700

    Plug Stitches into next.

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from 'next/app'
 
+import { globalStyles } from '../stitches.config'
+
 function MyApp({ Component, pageProps }: AppProps) {
+  // Set our global styles.
+  globalStyles()
+
   return <Component {...pageProps} />
 }
 export default MyApp
---
+++ web/pages/_document.tsx
@@ -0,0 +1,27 @@
+import NextDocument, { Head, Html, Main, NextScript } from 'next/document'
+
+import { getCssText } from '../stitches.config'
+
+class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <style
+            id="stitches"
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{ __html: getCssText() }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default Document
