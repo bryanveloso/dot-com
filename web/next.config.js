2e24c908b69b3f723abf0b539157a55f4b4a2a01
Bryan <bryan@avalonstar.com>
2021-09-25 23:21:58 -0700

    Add MDX support.

--- web/next.config.js
+++ web/next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+const withMDX = require('@next/mdx')()
+
+module.exports = withMDX({
+  reactStrictMode: true
+})
